39ae6fe6e30c677bc60a11d5467df0bcc259c3d7
Stephenmuhindi <bteneson42@gmail.com>
2024-07-01 06:05:50 -0700

    task 0

---
+++ utils/redis.js
@@ -0,0 +1,45 @@
+import { createClient } from 'redis';y
+
+import { promisify } from 'util';
+
+// we define various command and error msg
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  // here we test out the connection status
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  // this is the place where key values are gotten from
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
+
+  // redis key-val is set here
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
+
+  // key-val deletion happens here
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
